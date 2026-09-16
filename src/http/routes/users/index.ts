1bdaf93473a38143596312970b7ac5ec53b79369
tatyanepgoncalves <tatygoncalves715@gmail.com>
2026-09-16 20:13:33 -0300

    feat: register login user route in userRoutes

--- src/http/routes/users/index.ts
+++ src/http/routes/users/index.ts
@@ -1,12 +1,14 @@
 import type { FastifyInstance } from 'fastify'
 import { createUserRoute } from './createUserRoute.ts'
 import { getUserProfileRoute } from './getUserProfileRoute.ts'
+import { loginUserRoute } from './loginUserRoute.ts'
 import { logoutUserRoute } from './logoutUserRoute.ts'
 
 // biome-ignore lint/suspicious/useAwait: it not necessary
 export async function userRoutes(app: FastifyInstance) {
   // ROTAS PÚBLICAS
   app.register(createUserRoute)
+  app.register(loginUserRoute)
 
   // ROTAS COM AUTENTICAÇÃO NECESSÁRIA
   app.register(getUserProfileRoute)
