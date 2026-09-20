78385d0070833b119c5a2a5309b0b345389623c7
tatyanepgoncalves <tatygoncalves715@gmail.com>
2026-09-20 12:00:55 -0300

    fix: update import statement in drizzle.config.ts to use .ts extension

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
-import { env } from './src/config/env.js'
+import { env } from './src/config/env.ts'
 
 export default defineConfig({
   dbCredentials: {
